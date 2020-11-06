1ad71b8abe87b47636f6ee0c1e3dec0326f63e6d
Aleksey <73328416+k0r1tsa@users.noreply.github.com>
2020-11-06 14:54:31 +0700

    Добавление файлов "Практическая 2"
    Добавление файлов, практическая 2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
+#include "mydialog.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MyDialog w;
     w.show();
     return a.exec();
 }
